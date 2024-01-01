10d6e71bf7b8b8ce76b48dffa7f02addee6c7548
kir <kirpro95@yandex.ru>
2024-01-01 15:40:44 +0300

    6.6.0. Двумерный массив [#226796]

---
+++ src/main/java/ru/job4j/array/Sequence.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class Sequence {
+    public static void main(String[] args) {
+        int[][] doubleArray = new int[3][3];
+        doubleArray[0][0] = 1;
+        doubleArray[0][1] = 2;
+        doubleArray[0][2] = 3;
+        doubleArray[0][3] = 4;
+        doubleArray[1][0] = 5;
+        doubleArray[1][1] = 6;
+        doubleArray[1][2] = 7;
+        doubleArray[1][3] = 8;
+        doubleArray[2][0] = 9;
+    }
+}
